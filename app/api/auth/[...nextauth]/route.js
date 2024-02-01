d61f0ea2ca1821272ba4be18cbb1538debb3a2fb
Joseph Abboud <j.abboud1@hotmail.com>
2024-02-01 17:38:22 -0300

    Google Auth & MongoDB 100% Functional :) - 1.50 of Ttrl

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+import User from "@models/user";
 import { connectToDB } from "@utils/database";
 
 const handler = NextAuth({
@@ -10,20 +11,40 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {},
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({
+        email: session.user.email,
+      });
 
-  async signIn({ profile }) {
-    try {
-      await connectToDB();
+      session.user.id = sessionUser._id.toString();
 
-      // check if the user has an account, aka already exists
+      return session;
+    },
 
-      // if not, then create a new use
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
 
-      return true;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
+        // check if the user has an account, aka already exists
+        const userExists = await User.findOne({ email: profile.email });
+        // if not, then create a new use
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
   },
 });
+
+export { handler as GET };
+export { handler as POST };
