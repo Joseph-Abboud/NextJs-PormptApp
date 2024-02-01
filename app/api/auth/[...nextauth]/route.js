358ab5f5bd51183effe2ad298b9aa34642e62165
Joseph Abboud <j.abboud1@hotmail.com>
2024-02-01 15:55:14 -0300

    created MongoDB and setting User Database

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+import { connectToDB } from "@utils/database";
+
 const handler = NextAuth({
   providers: [
     GoogleProvider({
@@ -12,6 +14,16 @@ const handler = NextAuth({
 
   async signIn({ profile }) {
     try {
-    } catch (error) {}
+      await connectToDB();
+
+      // check if the user has an account, aka already exists
+
+      // if not, then create a new use
+
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
   },
 });
