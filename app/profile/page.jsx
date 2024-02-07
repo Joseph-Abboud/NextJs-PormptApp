e11586701a9a4b15a613830a4de3a0cef0dffbfb
Joseph Abboud <j.abboud1@hotmail.com>
2024-02-07 17:52:05 -0300

    Project completed - Only extra tasks left at 3.06

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -7,13 +7,14 @@ import { useRouter } from "next/navigation";
 import Profile from "@components/Profile";
 
 const MyProfile = () => {
+  const router = useRouter();
   const { data: session } = useSession();
 
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const response = await fetch("/api/users/${session?.user.id}/posts");
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
       const data = await response.json();
 
       setPosts(data);
@@ -22,9 +23,23 @@ const MyProfile = () => {
     if (session?.user.id) fetchPosts();
   }, []);
 
-  const handleEdit = () => {};
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
 
-  const handleDelete = async () => {};
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm("Are you sure you want to delete?");
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, { method: "DELETE" });
+
+        const filteredPosts = posts.filter((p) => p._id !== post._id);
+
+        setPosts(filteredPosts);
+      } catch (error) {}
+    }
+  };
 
   return (
     <Profile
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -6,6 +6,10 @@ import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const { data: session } = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
+
   const [copied, setCopied] = useState("");
 
   const handleCopy = () => {
@@ -52,10 +56,34 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
       <p
         className="font-inter text-sm blue_gradient cursor-pointer"
-        onClick={() => handleClick && handleTagClick(post.tag)}
+        onClick={() => handleTagClick && handleTagClick(post.tag)}
       >
-        {post.tag}
+        #{post.tag}
       </p>
+
+      {/* Below we are checking if the user that is logged is the 
+      same user that created the card, if so edit buttons appear!  */}
+
+      {session?.user.id === post.creator._id && pathName === "/profile" && (
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+          <p
+            className="font-inter text-sm green_gradient cursor-pointer"
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          {session?.user.id === post.creator._id && pathName === "/profile" && (
+            <div>
+              <p
+                className="font-inter text-sm orange_gradient cursor-pointer"
+                onClick={handleDelete}
+              >
+                Delete
+              </p>
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 };
